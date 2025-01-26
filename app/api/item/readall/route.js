27a3e22396813437a77e5a5108376ecece7daffc
sei <yyy.sei@icloud.com>
2025-01-27 00:28:26 +0900

    [Add] readall/route

---
+++ app/api/item/readall/route.js
@@ -0,0 +1,17 @@
+// app/api/item/create/route.js
+
+import { NextResponse } from "next/server"
+import connectDB from "../../../utils/database"
+import { ItemModel } from "../../../utils/schemaModels"
+
+export async function GET(){
+    try{
+        await connectDB()
+        const allItems = await ItemModel.find()
+        return NextResponse.json({message: "アイテム読み取り成功(オール)" , allItems: allItems})
+    }catch(err){
+        return NextResponse.json({message: "アイテム読み取り失敗(オール)"})
+    }
+}
+
+export const revalidate = 0 //Vercelでのデプロイの際に追加したコード
